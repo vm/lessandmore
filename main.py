434051d73b41cfe111305d45f6d9aeeba5f97634
Vignesh Mohankumar <vignesh@vigneshmohankumar.com>
2016-01-09 16:27:34 -0500

    Readability

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from flask import Flask, request, render_template
+from readability import ParserClient
+
+from convert import convert
 
 app = Flask(__name__)
+
+TOKEN = ''
+parser = ParserClient(TOKEN)
 
 @app.route('/')
 def enter_text():
@@ -9,11 +15,16 @@ def enter_text():
 
 @app.route('/', methods=['POST'])
 def post_text():
-    text = request.form['text']
-    less = request.form['less'].split(' ')
-    more = request.form['more'].split(' ')
-    content = text
-    return render_template('converted.html', content=content)
+    link = request.form['article']
+    less_words = request.form['less'].split(' ')
+    more_words = request.form['more'].split(' ')
+    response = parser.get_article(link).json()
+    converted_content = convert(response['content'], less_words, more_words)
+    return render_template(
+        'converted.html',
+        title=response['title'],
+        author=response['author'],
+        content=converted_content)
 
 
 if __name__ == '__main__':
