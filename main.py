1a42d433f0e2261ca628630b360c440c2dc8c16e
Vignesh Mohankumar <vignesh@vigneshmohankumar.com>
2016-01-10 10:03:52 -0500

    jquery

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import re
 
+from bs4 import BeautifulSoup
 from flask import Flask, request, render_template
-from readability import ParserClient
+import requests
 
 app = Flask(__name__)
-
-TOKEN = 'ee71317c7d5f30a39e53a83dd84a7646977e01c4'
-parser = ParserClient(token=TOKEN)
 
 @app.route('/')
 def converted():
@@ -14,14 +12,9 @@ def converted():
     more = request.args.get('more', '').split(' ')
     url = request.args.get('url', 'http://espn.go.com/chalk/story/_/id/14521633/daily-fantasy-mark-cuban-invests-dfs-analytics-company-fantasy-labs')
 
-    response = parser.get_article(url).json()
-    converted_content = re.sub('<[^<]+?>', '', response['content'])
-
-    return render_template(
-        'converted.html',
-        title=response['title'],
-        author=response['author'],
-        content=converted_content)
+    body = BeautifulSoup(requests.get(url).text).find('body').get_text()
+    print body
+    return replace_body(body)
 
 
 def convert_body(texts):
