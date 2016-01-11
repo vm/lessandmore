4955f0a305542f7469d9d7ebba189b15edd100ec
Vignesh Mohankumar <vignesh@vigneshmohankumar.com>
2016-01-10 23:02:16 -0500

    Remove unnecessary code

--- main.py
+++ main.py
@@ -9,15 +9,23 @@ from model import Transformer
 app = Flask(__name__)
 t = Transformer(threshold=0.4)
 
+NYTIMES_ARTICLE = (
+    'http://www.nytimes.com/2016/01/10/world/americas/el-chapo-mexican-drug-'
+    'lord-interview-with-sean-penn.html?hp&action=click&pgtype=Homepage'
+    '&clickSource=story-heading&module=a-lede-package-region&region=top-news'
+    '&WT.nav=top-news')
+
+
 @app.route('/')
 def enter_text():
     return render_template('index.html')
+
 
 @app.route('/', methods=['POST'])
 def converted():
     less = request.form.get('less', 'drugs').split(' ')
     more = request.form.get('more', 'candy').split(' ')
-    url = request.form.get('url', 'http://www.nytimes.com/2016/01/10/world/americas/el-chapo-mexican-drug-lord-interview-with-sean-penn.html?hp&action=click&pgtype=Homepage&clickSource=story-heading&module=a-lede-package-region&region=top-news&WT.nav=top-news')
+    url = request.form.get('url', NYTIMES_ARTICLE)
 
     soup = BeautifulSoup(requests.get(url).text)
 
@@ -25,20 +33,13 @@ def converted():
         script.extract()
     text = soup.get_text()
 
-    replacers = [
-        convert(x.strip(), less, more)
-        for x in re.split('Mr. |\.|\n|,', text)
-        if x
-    ]
-
     fixed = unicode(soup)
-    for text, new_text in replacers:
-        fixed = fixed.replace(text, new_text)
+    chunks = re.split('Mr. |\.|\n|,', text)  # LOL, really?
+    for chunk in chunks:
+        if chunk:
+            fixed = fixed.replace(chunk, t.transform(chunk, less, more))
     return fixed
 
-
-def convert(text, less, more):
-    return text, t.transform(text, less, more)
 
 if __name__ == '__main__':
     app.run(debug=True)
